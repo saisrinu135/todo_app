ba220a077e3af0b1710223f1adad347be9e2c0d4
saisrinu <saisrinugampa135@gmail.com>
2023-11-20 22:58:01 +0530

    'favicon'

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class taks_admin(admin.ModelAdmin):
+    list_display = ['user','task','completed','created_at']
+
+admin.site.register(tasks_model, taks_admin)
--- todoproject/settings.py
+++ todoproject/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_TASKS = os.path.join(os.path.join(BASE_DIR, 'tasks'),'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['STATIC_TASKS']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
